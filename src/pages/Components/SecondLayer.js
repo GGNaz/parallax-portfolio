b1066599459aa53a69f4451cb37ff56519c46628
Nazer Somera <nazersomera123@gmail.com>
2023-01-23 16:37:16 +0800

    savesss

--- src/pages/Components/SecondLayer.js
+++ src/pages/Components/SecondLayer.js
@@ -1,17 +1,38 @@
-import { ParallaxLayer } from '@react-spring/parallax'
-import React from 'react'
-
+import { ParallaxLayer } from "@react-spring/parallax";
+import Image from "next/image";
+import React from "react";
+import snakeandladderImg from "../../assets/snakeandladder.JPG";
+import styles from '../../styles/Home.module.css'
 function SecondLayer() {
+  const alignCenter = { display: 'flex', alignItems: 'center' }
   return (
-    <div>
-        <ParallaxLayer offset={1} speed={1} className="bg-[#2D2E31] h-screen ">
-            {/* <ParallaxLayer offset={1}> */}
-            <ParallaxLayer className="text-white text-4xl flex justify-center items-center">My Recent <span className='text-colorTheme'>Project</span></ParallaxLayer>
-            {/* </ParallaxLayer> */}
-          
-        </ParallaxLayer>
+    <>
+      {/* <ParallaxLayer offset={1} speed={1} className="bg-[#2D2E31] h-screen w-full flex flex-row"> */}
+      <ParallaxLayer sticky={{ start: 1, end: 3 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
+    {/* <div className={`${styles.card} ${styles.sticky}`}> */}
+    My Recent<span className="text-colorTheme ml-2"> Project</span>
+    {/* </div> */}
+  </ParallaxLayer>
+
+  <ParallaxLayer offset={1.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+    <div className={`${styles.card} ${styles.parallax} ${styles.purple}`}>
+    <Image
+            src={snakeandladderImg}
+            alt="snakeandladderImg"
+            // className="h-[70vh] w-[70vh] z-40"
+          />
     </div>
-  )
+  </ParallaxLayer>
+
+  <ParallaxLayer offset={2.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+    <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
+      <p>Neither am I</p>
+    </div>
+  </ParallaxLayer>
+     
+      {/* </ParallaxLayer> */}
+    </>
+  );
 }
 
-export default SecondLayer
+export default SecondLayer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,26 +8,62 @@ import tailwindIcon from "../assets/tailwind.png";
 import tagIcon from "../assets/tag.png";  
 import FirstLayer from "./Components/FirstLayer";
 import SecondLayer from "./Components/SecondLayer";
+import styles from '../styles/Home.module.css'
+import snakeandladderImg from "../assets/snakeandladder.JPG";
 export default function Home() {
   const ref = useRef();
+  const alignCenter = { display: 'flex', alignItems: 'center' }
   return (
     <div >
       {/* bg-[#2D2E31] */}
-      <Parallax pages={2} className="overflow-y-scroll no-scrollbar bg-darkColor">
-        {/* <ParallaxLayer offset={0} >
-          <ParallaxLayer offset={0} speed={1} className="bg-red-600 flex justify-center items-center">
-            <h2>Welcome to my website</h2>
-            </ParallaxLayer>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} className="bg-blue-600">
-            <h2>Web development is fun!</h2>
-        </ParallaxLayer> */}
-        <FirstLayer/>
-        <SecondLayer/>
-        {/* <ParallaxLayer offset={0.2} speed={0.5}>
-            
-        </ParallaxLayer>  */}
-      </Parallax>
+      {/* <Parallax pages={5} className="overflow-y-scroll no-scrollbar">
+    
+        <FirstLayer/> 
+   <SecondLayer/>
+     
+      </Parallax> */}
+
+
+<Parallax pages={5} className="overflow-y-scroll no-scrollbar bg-darkColor">
+  {/* <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
+    <p className={styles.scrollText}>Scroll down</p>
+  </ParallaxLayer> */}
+    <FirstLayer/> 
+    {/* <SecondLayer/> */}
+
+  <ParallaxLayer sticky={{ start: 1, end: 2 }} className="flex justify-start items-center z-50 ">
+  {/* <ParallaxLayer sticky={{ start: 1, end: 2 }}>Bag</ParallaxLayer> */}
+    <div className="flex flex-col justify-start text-darkColor bg-white w-[50%] items-center p-5">
+      <div >
+    <div className="text-6xl font-semibold">My</div><div className="text-6xl font-semibold">Recent</div> <div className="text-colorTheme text-6xl font-semibold"> Projects</div>
+    </div>
+    </div>
+  </ParallaxLayer>                                            
+ 
+  <ParallaxLayer offset={1} speed={0.5} className="flex justify-end items-center" >
+    {/* <div className={`${styles.card} ${styles.parallax} ${styles.purple}`}> */}
+    <div className="flex justify-center w-[50%] p-5 bg-orange-500">
+    <Image
+            src={snakeandladderImg}
+            alt="snakeandladderImg"
+            className="h-[70vh] w-[70vh]"
+          />
+    </div>
+  </ParallaxLayer>
+
+  <ParallaxLayer offset={2} speed={0.5} className="flex justify-end items-center bg-blue-600">
+    {/* <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}> */}
+    <div className="flex justify-center w-[50%] p-5 bg-orange-500">
+    <Image
+            src={snakeandladderImg}
+            alt="snakeandladderImg"
+            className="h-[70vh] w-[70vh]"
+          />
+    </div>
+    {/* </div> */}
+  </ParallaxLayer>
+
+</Parallax>
     </div>
   );
 }
